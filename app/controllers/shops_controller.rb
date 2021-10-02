85d6c5eaeb9b85dbc27be22b1e41f2958d3631be
Alexandre Morey <alexander006_9@hotmail.com>
2021-10-02 21:24:38 +0200

    Shops controller show action

--- app/controllers/shops_controller.rb
+++ app/controllers/shops_controller.rb
@@ -1,2 +1,12 @@
 class ShopsController < ApplicationController
+  before_action :set_shop, only: :show
+
+  def show
+  end
+
+  private
+
+  def set_shop
+    @shop = Shop.find(params[:id])
+  end
 end
