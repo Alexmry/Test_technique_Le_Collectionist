0856effb767caa197a58d5950e4628272595ded8
Alexandre Morey <alexander006_9@hotmail.com>
2021-10-02 22:08:10 +0200

    edit action in days controller + edit form + link from the show page to the edit form

--- app/controllers/days_controller.rb
+++ app/controllers/days_controller.rb
@@ -1,2 +1,18 @@
 class DaysController < ApplicationController
+  before_action :find_shop, except: :edit
+
+  def update
+  end
+
+  def edit
+    @day = Day.find(params[:id])
+    @shop = Shop.find(@day.shop_id)
+  end
+
+  private
+
+  def find_shop
+    @shop = Shop.find(params[:shop_id])
+  end
+
 end
