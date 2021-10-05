b9225f8fcf5c2f2aa932ae99644c308b208ffe5f
Alexandre Morey <alexander006_9@hotmail.com>
2021-10-05 17:19:40 +0200

    added the sot display method in the shops show page but still need to fixe the method in the Day model

--- app/models/day.rb
+++ app/models/day.rb
@@ -1,4 +1,20 @@
 class Day < ApplicationRecord
   belongs_to :shop
   has_many :slots, dependent: :destroy
+  # jour actuel en premier, et en gras
+
+  def display_slot(day)
+    morning = day.slots.all
+    #day.slots.all.Time.now.strftime("%H:%M")
+  end
 end
+
+
+# opening_morning = day.slots.all
+# closing_morning = day.slots.all
+# opening_afternoon = day.slots.all
+# closing_afternoon = day.slots.all
+
+# puts "#{opening_morning} - #{closing_morning}, #{opening_afternoon} - #{closing_afternoon}"
+
+# #day.slots.all.Time.now.strftime("%H:%M")
--- app/models/shop.rb
+++ app/models/shop.rb
@@ -1,9 +1,3 @@
 class Shop < ApplicationRecord
   has_many :days, dependent: :destroy
-
-  # jour actuel en premier, et en gras
-
-  # def display_slot(day)
-  # end
-
 end
